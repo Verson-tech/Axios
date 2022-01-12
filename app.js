63322dfda023ca5f9c7f1cdf7531c59a08c18770
vira.murray <vira.avramenko@gmail.com>
2022-01-12 13:32:52 +0200

    setting headers with Axios

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-axios.get('https://api.cryptonator.com/api/full/btc-usd')
-.then (res=>{
-    console.log(res.data.ticker.price)
-})
-.catch(err=>{
-    console.log('ERROR!', err)
-})
+axios
+  .get("https://api.cryptonator.com/api/full/btc-usd")
+  .then((res) => {
+    console.log(res.data.ticker.price);
+  })
+  .catch((err) => {
+    console.log("ERROR!", err);
+  });
 
 const fetchBitcoinPrice = async () => {
-    try {
-        const res = await axios.get('https://api.cryptonator.com/api/full/btc-usd')
-        console.log(res.data.ticker.price)
-    }catch(e){
-        console.log('ERROR!', e)
-    }
-}
+  try {
+    const res = await axios.get("https://api.cryptonator.com/api/full/btc-usd");
+    console.log(res.data.ticker.price);
+  } catch (e) {
+    console.log("ERROR!", e);
+  }
+};
+
+
+// setting headers with Axios
+//https://icanhazdadjoke.com/api
+const getDadJoke = async () => {
+  const config = { headers: { Accept: "application/json" } };
+  const res = await axios.get("https://icanhazdadjoke.com/", config);
+  console.log(res);
+};
